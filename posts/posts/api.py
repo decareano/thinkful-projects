6c9da133b43ffcb560620928ae65c11ef80a8fdf
thefunkjunky <j.garrett.anderson@gmail.com>
2016-03-13 23:13:11 -0700

    Added single post endpoint

--- posts/posts/api.py
+++ posts/posts/api.py
@@ -18,3 +18,20 @@ def posts_get():
     data = json.dumps([post.as_dictionary() for post in posts])
     return Response(data, 200, mimetype="application/json")
 
+@app.route("/api/posts/<int:id>", methods=["GET"])
+def posts_get(id):
+    """ Single post endpoint """
+    # Get the posts from the database
+    posts = session.query(models.Post).get(id)
+
+    # Check whether the post exists
+    # If not, return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+
