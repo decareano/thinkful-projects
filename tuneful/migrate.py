7193a1a199cb32244d5906a55bfe6deb439ef37b
Garrett Anderson <j.garrett.anderson@gmail.com>
2016-04-10 18:54:56 -0700

    Migrations successful.

--- tuneful/migrate.py
+++ tuneful/migrate.py
@@ -1,13 +1,20 @@
+# NOTE: After running migrate.py db init, go to migrations/env.py and changed
+# line 22 to :
+# current_app.config.get('DATABASE_URI').replace("%", "%%"))
+# in order to use correct URI variable for this project and % signs escaped with %%
+
 from flask_script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
-from .database import Base
-from .main import app
+from tuneful.main import app
+from tuneful.database import Base
 
 class DB(object):
     def __init__(self, metadata):
         self.metadata = metadata
+        
 
+manager = Manager(app)
 migrate = Migrate(app, DB(Base.metadata))
 manager.add_command('db', MigrateCommand)
 
